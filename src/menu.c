4af1cebf304a2d100b5c6060523b0a524b4cec12
Nashspinaice <nashspinaice60699@mail.ru>
2018-09-17 23:26:38 +0700

    Add menu.c

---
+++ src/menu.c
@@ -0,0 +1,15 @@
+#include "Vshuman"
+#include "Vscomp"
+
+void menu (){
+printf ("World of batleships \n Please chose type of game \n 1 : 2 players \n 2 : VS Computer\n");
+int n;
+scanf("%d", &n);
+switch(n){
+case 1: system("CLS");
+Vshuman(); break;
+case 2: system("CLS");
+Vscomp(); break;
+default : system("CLS");
+menu();
+} 
